bc77af74dab61ff7175670ef150535dcc2dd3d66
Mustafa Celik <celikmus@gmail.com>
2016-10-13 12:53:00 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,12 @@
+# Graffiti express example
+
+### Run
+
+```bash
+# MongoDB has to be running
+# Install dependencies
+yarn install
+# Run all servers
+yarn run
+# Express server listening on port 3001
+```
---
+++ src/express.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import { json } from 'body-parser';
+import graffiti from '@risingstack/graffiti';
+import schema from './schema';
+
+const app = express();
+
+app.use(json());
+
+app.use(graffiti.express({
+  schema
+}));
+
+// redirect all requests to /graphql
+// to open GraphiQL by default
+app.use((req, res) => {
+  res.redirect('/graphql');
+});
+
+app.listen(3001, (err) => {
+  if (err) {
+    throw err;
+  }
+
+  console.log('Express server is listening on port 3001');
+});
